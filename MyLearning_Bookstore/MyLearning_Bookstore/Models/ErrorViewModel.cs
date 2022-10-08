aeb7dd069ab3e8cbe170eb32ee591e1ebda8a6f2
shrey <shrey.kanani@darshan.ac.in>
2022-10-08 09:56:50 +0530

    Created Project

---
+++ MyLearning_Bookstore/MyLearning_Bookstore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyLearning_Bookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
